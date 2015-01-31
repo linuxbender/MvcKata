88646a62416b5a1752e199be1bf22a6c3d24db7f
daniel glenn <linuxbender@gmail.com>
2015-01-31 03:15:09 +0100

    setup for wireddep and inject

--- GulpAutomation/gulp.config.js
+++ GulpAutomation/gulp.config.js
@@ -2,11 +2,35 @@ module.exports = function () {
     'use strict';
     
     var client = './';
+    var clientApp = client + 'Scripts/App/';
+    var tmp = './tmp/';
     
     var config = {
-        tmp: './tmp/',
+        tmp: tmp,
         allJs: ['Scripts/App/**/*.js', './*.js'],
-        sass: client + 'Styles/app.scss'
+        client: client + 'Views/Shared/',
+        css: tmp + 'app.css',
+        index: client + 'Views/Shared/_Layout.cshtml',
+        js: [
+        	clientApp + '**/*.module.js',
+        	clientApp + '**/*.js',
+        	'!' + clientApp + '**/*.spec.js'
+        ],
+        sass: client + 'Styles/app.scss',
+        bower: {
+        	json: require('./bower.json'),
+        	directory: './bower_modules/',
+        	ignorePath: '../..'
+        }
+    };
+
+    config.getWiredepDefaultoptions = function() {
+    	var options = {
+    		bowerJson: config.bower.json,
+    		directory: config.bower.directory,
+    		ignorePath: config.bower.ignorePath
+    	};
+    	return options;
     };
     
     return config;
--- GulpAutomation/gulpfile.js
+++ GulpAutomation/gulpfile.js
@@ -1,14 +1,18 @@
 
 'use strict';
+
 var gulp = require('gulp');
 var args = require('yargs').argv;
 var del = require('del');
 
 // config file
 var config = require('./gulp.config')();
+
 // load all gulp plugin lazy
 var $ = require('gulp-load-plugins')({lazy: true});
 
+//
+// Main Task
 gulp.task('vet', function () {
     
     log('Analyzing source with JSHint and JSCS');
@@ -20,9 +24,10 @@ gulp.task('vet', function () {
 	.pipe($.jshint())
 	.pipe($.jshint.reporter('jshint-stylish', {verbose: true}))
     .pipe($.jshint.reporter('fail'));
-    
 });
 
+//
+// Tasks for Sass and Css
 gulp.task('styles', ['clean-styles'], function() {
     
     log('Comiling Sass to Css');
@@ -33,7 +38,6 @@ gulp.task('styles', ['clean-styles'], function() {
     .on('error',errorLogger)
     .pipe($.autoprefixer({browsers:['last 2 version', '> 5%']}))
     .pipe(gulp.dest(config.tmp));
-    
 });
 
 gulp.task('clean-styles', function(done) {
@@ -45,6 +49,27 @@ gulp.task('clean-styles', function(done) {
 gulp.task('sass-watcher', function() {
     
     gulp.watch([config.sass],['styles']);
+});
+
+//
+// wire + inject libs
+gulp.task('wiredep', function() {
+    var options = config.getWiredepDefaultoptions();
+    var wiredep = require('wiredep').stream;
+
+    return gulp
+    .src(config.index)
+    .pipe(wiredep(options))
+    .pipe($.inject(gulp.src(config.js)))
+    .pipe(gulp.dest(config.client));
+});
+
+gulp.task('inject',['wiredep','styles'], function() {
+
+    return gulp
+    .src(config.index)
+    .pipe($.inject(gulp.src(config.css)))
+    .pipe(gulp.dest(config.client));
 });
 
 // 
