285989e92c3819c958386a683bd6cbacd3fd8931
daniel glenn <linuxbender@gmail.com>
2015-02-21 20:38:48 +0100

    Rename Tests

--- DiResolver.Tests/Bootstrapper/ContainerConfigInterceptorTests.cs
+++ DiResolver.Tests/Bootstrapper/ContainerConfigInterceptorTests.cs
@@ -27,7 +27,7 @@ using NUnit.Framework;
 
 namespace DiResolver.Tests.Bootstrapper
 {
-    public class ContainerConfigInterceptor : BaseConfig
+    public class ContainerConfigInterceptorTests : BaseConfig
     {
         [Test]
         public void Given_Unity_Register_AuditProvider_Exceptet_To_Intercept_Method_Call()
