1ae8369b4910a43a31f4c765a782be0639d121fc
daniel glenn <linuxbender@gmail.com>
2015-02-21 01:18:02 +0100

    init core module

--- GruntAutomation/App/Core/core.module.js
+++ GruntAutomation/App/Core/core.module.js
@@ -1,1 +1,20 @@
-﻿
+﻿(function () {
+    "use strict";
+    angular
+        .module("app.core", [
+        /*
+        * Angular modules
+        */
+        "ngAnimate",
+        "ngRoute",
+        "ngSanitize",
+        /*
+        * common modules
+        */
+        "common.logger",
+        "common.exception",
+        "common.offline",
+        "common.busyIndicator",
+        "common.localStorage"
+        ]);
+})();
